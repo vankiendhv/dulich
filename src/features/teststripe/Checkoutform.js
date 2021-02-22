41a042d62a339d3d2be243aa3d033ecd5a4a84f1
165TDV200196 <56056004+165TDV200196@users.noreply.github.com>
2021-02-22 08:18:37 +0700

    22/2/2021

--- src/features/teststripe/Checkoutform.js
+++ src/features/teststripe/Checkoutform.js
@@ -5,7 +5,7 @@ import stripeApi from '../../api/stripeApi';
 import { message } from 'antd';
 import "./card.css";
 import { useDispatch } from 'react-redux';
-import { addhoadon } from '../container/admin/Hoadon/hoadonSlice';
+import { addhoadon, hoadonData } from '../container/admin/Hoadon/hoadonSlice';
 import { useHistory } from 'react-router-dom';
 export default function CheckoutForm(props) {
     const stripe = useStripe();
@@ -16,6 +16,7 @@ export default function CheckoutForm(props) {
     console.log(hoadon);
     const dispatch = useDispatch();
     const history = useHistory();
+    const actionhoadon = async () => { await dispatch(hoadonData()) }
     const handleSubmit = async (event) => {
         event.preventDefault()
         if (!stripe || !elements) {
@@ -40,6 +41,7 @@ export default function CheckoutForm(props) {
             if (result.paymentIntent.status === 'succeeded') {
                 message.success("Thanh toán thành công!");
                 dispatch(addhoadon({ tourId: hoadon.tourId, userId: hoadon.userId, embe: hoadon.embe, treem: hoadon.treem, nguoilon: hoadon.nguoilon, ngaydi: hoadon.ngaydi }));
+                actionhoadon();
                 history.push(`/tour/${hoadon.tourId}`);
             }
         }
